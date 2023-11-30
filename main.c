0238a463ede7dc6709de650d3dab4a92f9249003
MilkyWayXDYT <152471759+MilkyWayXDYT@users.noreply.github.com>
2023-11-30 10:06:48 +0300

    Update main.c
    На русском пожалуйста пишите

--- main.c
+++ main.c
@@ -5,11 +5,11 @@ int main(int argc, char **argv)
 {
     char first[255], last[255];
 
-    printf("Enter your first name: ");
+    printf("Напишите своё имя: ");
     fgets(first, 255, stdin);
     first[strlen(first)-1] = '\0'; /* remove the newline at the end */
 
-    printf("Now enter your last name: ");
+    printf("Напишите свою фамилию: ");
     gets(last); /* buffer overflow? what's that? */
 
     printf("Hello %s %s!\n", first, last);
